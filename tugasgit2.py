a06bc48f9747f1c11691ed17d7f7388749e76a6a
BagasCaturS <bagascatursantoso@gmail.com>
2023-03-10 10:55:32 +0700

    v1.2 menambahkan pilihan nama agar memprint sisanya tugasgit2.py

--- tugasgit2.py
+++ tugasgit2.py
@@ -1,12 +1,17 @@
-user = {
+user = [
+    {'nama': 'arsene lupin', 'nik': 1234, 'jenis_kelamin': 'male', 'tanggal_lahir': '1902-03-23'},
+    {'nama': 'sherlock holmes', 'nik': 4321, 'jenis_kelamin': 'male', 'tanggal_lahir': '1876-08-16'},
+    {'nama': 'irene adler', 'nik': 6789, 'jenis_kelamin': 'female', 'tanggal_lahir': '1884-10-07'}
+]
 
-{nama: arsene lupin, nik: 1234, jenis_kelamin: male; tanggal_lahir:1902-03-23},
 
-{nama: sherlock holmes, nik: 4321, jenis_kelamin: male; tanggal_lahir:1876-08-16},
+user_input = input("Masukkan nama yang hendak dicari:")
 
-{nama: irene adler, nik:6789, jenis_kelamin: female; tanggal_lahir:1884-10-07},
+# jika apa yang diinout user ada maka tampilkan seluruh data. jika tidak ada maka "data kosong"
 
-}
+if user_input in user :
+    print ("nama yang dicari adalah",user[user_input])
+else :
+    print ((user), 'nama tidak ada')
 
-user = input("Masukkan nama yang hendak dicari:")
 
